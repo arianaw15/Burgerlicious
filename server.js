73de563945db0bda054cce4acbe3ea165bd212c5
Ariana Winters <arianaw15@gmail.com>
2021-05-25 15:48:06 -0600

    updating views

--- server.js
+++ server.js
@@ -1,46 +1,28 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const dotenv = require("dotenv");
-dotenv.config();
 
-// Create an instance of the express app.
-const app = express();
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-
-
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
-const db = require("./models");
+const app = express();
 
-// Set Handlebars as the default templating engine.
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname:'.hbs' }));
-app.set('view engine', 'hbs');
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
 
-const routes = require('./controller/burgers_controller');
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+const routes = require('./controller/burgers_controller.js');
+
 app.use(routes);
 
-
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, () =>
   console.log(`Server listening on: http://localhost:${PORT}`)
 );
-
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
-
-app.use((req, res) => {
-  res.status(404).render("error");
-});
