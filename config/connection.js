39e477e257c8119dfa3408eb9ba5354292bf0dbb
Ariana Winters <arianaw15@gmail.com>
2021-05-26 13:14:24 -0600

    finally following directions

--- config/connection.js
+++ config/connection.js
@@ -7,26 +7,13 @@ var Sequelize = require('sequelize');
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.Username,
-    process.env.Password,
-    process.env.Database,
-    {
-      host: 'bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
-
-// Set up our connection information
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: process.env.PASSWORD,
-//   database: 'burgers_db',
-// });
+  connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.PASSWORD,
+  database: 'burgers_db',
+});
 
 // Connect to the database
 connection.connect((err) => {
