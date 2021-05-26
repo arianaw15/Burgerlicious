f3c2202eca9ce349a8fcd0c37a0f3d76d2298e53
Ariana Winters <arianaw15@gmail.com>
2021-05-26 12:37:57 -0600

    updating with jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,29 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
 
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
+
 // Set up our connection information
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.PASSWORD,
-  database: 'burgers_db',
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: process.env.PASSWORD,
+//   database: 'burgers_db',
+// });
 
 // Connect to the database
 connection.connect((err) => {
